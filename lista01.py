d04b3b4e293b3849b6f3006188dac9d7093ac750
Lucas Eduardo dos Santos Machado <lucaseduardo168@gmail.com>
2024-07-12 09:05:18 -0300

    Adicionado exercício 6

--- lista01.py
+++ lista01.py
@@ -1,50 +1,58 @@
-# 1. Calculadora Simples: Solicite ao usu·rio para inserir dois valores numÈricos,
-# realize as operaÁıes de soma, subtraÁ„o, multiplicaÁ„o e divis„o, e ao final exiba
-# os valores de cada uma das operaÁıes.
+# 1. Calculadora Simples: Solicite ao usu√°rio para inserir dois valores num√©ricos,
+# realize as opera√ß√µes de soma, subtra√ß√£o, multiplica√ß√£o e divis√£o, e ao final exiba
+# os valores de cada uma das opera√ß√µes.
 
 
-# 2. Conversor de Temperatura: Solicite ao usu·rio um valor de temperatura em graus
-# Celsius, converta-a para Fahrenheit e exiba o resultado da convers„o.
+# 2. Conversor de Temperatura: Solicite ao usu√°rio um valor de temperatura em graus
+# Celsius, converta-a para Fahrenheit e exiba o resultado da convers√£o.
 
 
-# 3. ¡rea do CÌrculo: Solicite ao usu·rio um valor do raio de um cÌrculo, calcule sua
-# ·rea e exiba o resultado do c·lculo.
+# 3. √Årea do C√≠rculo: Solicite ao usu√°rio um valor do raio de um c√≠rculo, calcule sua
+# √°rea e exiba o resultado do c√°lculo.
 
 
-# 4. ¡rea do Tri‚ngulo: Solicite ao usu·rio um valor da base e da altura de um
-# tri‚ngulo, calcule sua ·rea e exiba o resultado do c·lculo.
+# 4. √Årea do Tri√¢ngulo: Solicite ao usu√°rio um valor da base e da altura de um
+# tri√¢ngulo, calcule sua √°rea e exiba o resultado do c√°lculo.
 
 
-# 5. Volume da Esfera: Solicite ao usu·rio um valor do raio de uma esfera, calcule seu
-# volume e exiba o resultado do c·lculo.
+# 5. Volume da Esfera: Solicite ao usu√°rio um valor do raio de uma esfera, calcule seu
+# volume e exiba o resultado do c√°lculo.
 
 
-# 6. Calculadora de MÈdia AritmÈtica: Solicite ao usu·rio para que ele insira trÍs
-# valores de notas, realize o c·lculo da mÈdia aritmÈtica e em seguida exiba os trÍs
-# valores digitados pelo usu·rio e o resultado do c·lculo.
+# 6. Calculadora de M√©dia Aritm√©tica: Solicite ao usu√°rio para que ele insira tr√™s
+# valores de notas, realize o c√°lculo da m√©dia aritm√©tica e em seguida exiba os tr√™s
+# valores digitados pelo usu√°rio e o resultado do c√°lculo.
+
+n1 = float(input("Digite o valor da primeira nota: "))
+n2 = float(input("Digite o valor da segunda nota: "))
+n3 = float(input("Digite o valor da terceira nota: "))
+
+ma = (n1+n2+n3)/3
+
+print(f"M√©dia aritim√©tica = {ma:.2f} \n As notas foram: {n1}, {n2}, {n3}")
 
 
-# 7. Calculadora de MÈdia Ponderada: Solicite ao usu·rio para que ele insira os
-# valores de 4 notas e seus respectivos pesos, em seguida realize o c·lculo da mÈdia
-# pondera e exiba o resultado do c·lculo.
+# 7. Calculadora de M√©dia Ponderada: Solicite ao usu√°rio para que ele insira os
+# valores de 4 notas e seus respectivos pesos, em seguida realize o c√°lculo da m√©dia
+# pondera e exiba o resultado do c√°lculo.
 
 
-# 8. EquaÁ„o de Segundo Grau: Solicite ao usu·rio os valores de ìaî, ìbî, ìcî e ìxî, em
-# seguida resolva uma equaÁ„o quadr·tica do tipo y = ax2 + bx + c e exiba o valor
-# de y para o usu·rio.
+# 8. Equa√ß√£o de Segundo Grau: Solicite ao usu√°rio os valores de ‚Äúa‚Äù, ‚Äúb‚Äù, ‚Äúc‚Äù e ‚Äúx‚Äù, em
+# seguida resolva uma equa√ß√£o quadr√°tica do tipo y = ax2 + bx + c e exiba o valor
+# de y para o usu√°rio.
 
 
-# 9. Calculadora de IMC: Solicite ao usu·rio os valores de peso (kg) e altura (m),
-# calcule o Ìndice de massa corporal (IMC), sabendo que IMC = peso/altura≤, em seguida
+# 9. Calculadora de IMC: Solicite ao usu√°rio os valores de peso (kg) e altura (m),
+# calcule o √≠ndice de massa corporal (IMC), sabendo que IMC = peso/altura¬≤, em seguida
 # exiba o valor do IMC calculado.
 
 
-# 10.Tabuada: Solicite ao usu·rio um valor numÈrico, em seguida, exiba a tabuada de
-# um n˙mero especÌfico (por exemplo, 5). O programa dever· ter como saÌda:
+# 10.Tabuada: Solicite ao usu√°rio um valor num√©rico, em seguida, exiba a tabuada de
+# um n√∫mero espec√≠fico (por exemplo, 5). O programa dever√° ter como sa√≠da:
 # 5x1 = 5; 5x2 = 10; 5x3 = 15; 5x4 = 20; 5x5 = 25; 5x6 = 30; 5x7 = 35; 5x8 = 40; 5x9
 # = 45; 5x10 = 50;
 
 
-# 11.Convers„o de Segundos para o Formato HORA:MINUTO:SEGUNDO: Solicite
-# ao usu·rio um valor numÈrico correspondente ‡ quantidade de segundos, em
+# 11.Convers√£o de Segundos para o Formato HORA:MINUTO:SEGUNDO: Solicite
+# ao usu√°rio um valor num√©rico correspondente √† quantidade de segundos, em
 # seguida converta o valor para o formato de HORA:MINUTO:SEGUNDO.
